43b6b3f95b14279d2bc9ebec2c38fe6df2e5f2be
Platon Pronko <platon7pronko@gmail.com>
2020-08-21 16:11:18 +0300

    fix one extra black pixel at the end of the chart

--- graph.c
+++ graph.c
@@ -53,8 +53,8 @@ gboolean graph_update(Graph* g) {
     for (int t = 0; t < g->width; t++) {
       gdouble ymin = round(stack[t] * g->height / max);
       gdouble ymax = round((stack[t] + meas->data[t]) * g->height / max);
-      cairo_move_to(cr, t - 0.5, ymin + 0.5);
-      cairo_line_to(cr, t - 0.5, ymax + 0.5);
+      cairo_move_to(cr, t + 0.5, ymin + 0.5);
+      cairo_line_to(cr, t + 0.5, ymax + 0.5);
       stack[t] += meas->data[t];
     }
     cairo_stroke(cr);
